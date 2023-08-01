4836aa4430302f20765287c4b0dc5805f87a6a67
JaimeOrtega1 <jortegat2@unemi.edu.ec>
2023-08-01 14:39:14 -0500

    actualizacion

--- django-backend/venta/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(GrupoCliente)
-admin.site.register(Cliente)
-admin.site.register(Venta)
-admin.site.register(VentaDetalle)
